328add0b7066a8602ddccc1bbd931b98f90a4bd8
Ubiquity1776 <osoayotundeemmanuel@gmail.com>
2023-02-20 08:27:59 -0800

    puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,12 +6,6 @@
  *
  *Return :Always 0(success)
  */
-=======
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
->>>>>>> b642f9dac2347572b9d169b302f7cbd71ab98fb7
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
